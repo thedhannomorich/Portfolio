0a4454437f67a679ac29d488cbe06d5df72b6362
thedhannomorich <itsmeronggonn@gmail.com>
2026-06-05 01:24:24 +0600

    Update blogs.js

--- blogs.js
+++ blogs.js
@@ -1,18 +1,10 @@
 export const blogs = [
   {
-    id: "welcome-post",
+    id: "news",
     dateTime: "2026-06-05T20:30:00+06:00",
-    text: "Welcome to my renewed personal website. This is my first post here.",
+    text: "Google is currently leading the AI market, and Google I/O clearly showed why. The company introduced major advancements across Gemini, Search, Android, Workspace, and AI-generated video, demonstrating that AI is now at the center of everything Google does. What makes Google stand out is not just the quality of its AI models, but also its ability to bring AI to billions of users through products people already use every day. While competition remains strong, Google’s latest announcements show that it has turned its AI vision into real products at a massive scale, putting it in a leading position in the industry.",
     images: [
-      "https://thedhannomorich.github.io/Atlas/logo.png"
+      "https://helios-i.mashable.com/imagery/articles/05bfbvWPRwL6Zin0Ie2gcfy/hero-image.fill.size_1200x675.v1765277753.jpg"
     ]
   },
-  {
-    id: "atlas-post",
-    dateTime: "2026-06-04T18:15:00+06:00",
-    text: "I built Atlas because I wanted an AI study assistant that actually understands how students learn.",
-    images: [
-      "https://thedhannomorich.github.io/Atlas/logo.png"
-    ]
-  }
 ];
